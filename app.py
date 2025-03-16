13c5a5a71a7792e53173367e7a6665e005138bc5
devanshvpurohit <devanshvpurohit@gmail.com>
2025-03-16 20:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ authenticator = stauth.Authenticate(
     config["cookie"]["key"], config["cookie"]["expiry_days"]
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", location="sidebar")
+
 
 if authentication_status is False:
     st.error("Username/password is incorrect")
